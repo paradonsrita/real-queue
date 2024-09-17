083bbecaf9cfcb1e3c7ded817a3c95f1c13f8cb3
Paradon Srita <paradonsrita2003@gmail.com>
2024-09-17 16:34:27 +0700

    last-before-presentation-on-17-9-67

---
+++ QueueCounter/Models/QueueModel.cs
@@ -0,0 +1,14 @@
+﻿namespace QMS.Models
+{
+    public class QueueModel
+    {
+        public int queue_id { get; set; }
+        public DateTime queue_date { get; set; }
+        public string QueueStatus { get; set; }
+        public string QueueType { get; set; }
+
+        public string QueueNumber { get; set; }
+
+        public int counter { get; set; }
+    }
+}

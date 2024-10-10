dc2780cc9b9e0178e3bc20224af769dc3d40c6bb
Paradon Srita <paradonsrita2003@gmail.com>
2024-10-10 10:04:52 +0700

    print-booking

--- QueueUser/obj/Debug/net7.0/QMS.AssemblyInfo.cs
+++ QueueUser/obj/Debug/net7.0/QMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ed139c11576064666df1fc0e6c2d91d839e45d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1158229d9b31d2c1a685742bfe7952e118089cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("QMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ QueueUser/wwwroot/js/Print.js
@@ -0,0 +1,19 @@
+﻿function printBooking(queueNumber, queueType, bookingDate, appointmentDate, phoneNumber) {
+    const printContent = `
+        <div style="margin: 0;">
+            <h2>ข้อมูลการจองคิว</h2>
+            <p>หมายเลขคิว: ${queueNumber}</p>
+            <p>ประเภทคิว: ${queueType}</p>
+            <p>วันที่จอง: ${new Date(bookingDate).toLocaleDateString()}</p>
+            <p>วันที่นัดหมาย: ${new Date(appointmentDate).toLocaleDateString()}</p>
+            <p>หมายเลขโทรศัพท์: ${phoneNumber}</p>
+        </div>
+    `;
+
+    const printWindow = window.open('', '', 'height=600,width=800');
+    printWindow.document.write('<html><head><title>ปริ้นการจองคิว</title></head><body>');
+    printWindow.document.write(printContent);
+    printWindow.document.write('</body></html>');
+    printWindow.document.close();
+    printWindow.print();
+}
